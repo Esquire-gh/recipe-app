48a5aee4133d2f8777e503aac2b62ff66c88db3c
Richard Ackon <ackon.k.richard@gmail.com>
2021-01-31 01:47:47 +0000

    Display recipe-tem details when you click one of them in the recipe list section

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,18 +8,23 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeItemSelected: EventEmitter<Recipe> = new EventEmitter()
   recipes: Recipe[];
 
   constructor() {
     this.recipes = [
       new Recipe("Fufu", "recipe for fufu", "https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400"),
-      new Recipe("Fufu", "recipe for fufu", "https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400"),
-      new Recipe("Fufu", "recipe for fufu", "https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400"),
+      new Recipe("Banku", "recipe for Banku", "https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400"),
+      new Recipe("Kenkey", "recipe for Keney", "https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400"),
 
     ]
   }
 
   ngOnInit(): void {
+  }
+
+  selectRecipeItemForDisplay(recipe: Recipe) {
+    this.recipeItemSelected.emit(recipe)
   }
 
   addNewRecipe(){
