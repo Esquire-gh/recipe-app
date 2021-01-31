9337c3227c4647ee114b2a7f801e0b13c40f46ee
Richard Ackon <ackon.k.richard@gmail.com>
2021-01-31 02:00:07 +0000

    Add button in shopping-list-edit for adding new ingredients to shopping list

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, ViewChild,EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/common/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('itemName') itemName: ElementRef;
+  @ViewChild('itemAmount') itemAmount: ElementRef;
+  @Output() newIngredientAdded: EventEmitter<Ingredient> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    let shoppingListItem: Ingredient = new Ingredient(
+      this.itemName.nativeElement.value, 
+      this.itemAmount.nativeElement.value
+    )
+    this.newIngredientAdded.emit(shoppingListItem)
   }
 
 }
--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -21,4 +21,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addNewIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+  }
+
 }
